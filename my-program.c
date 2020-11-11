d7026d11b04011e4f96264963221962296924247
realcxc <53243394+realcxc@users.noreply.github.com>
2020-11-11 18:08:43 +0800

    1

--- my-program.c
+++ my-program.c
@@ -2,86 +2,131 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+int matrix[6][6]={ 1,-1,-1,-1, 1, 1,
+                   1, 1,-1,-1, 1, 1,
+                   1, 1, 2, 2, 1, 1,
+                  -1,-1,-1,-1, 0, 2,
+                   1, 1, 2, 2, 1, 1,
+                  -1,-1,-1,-1, 2, 2,
+};
+int get_priority(char a,char b){
+    int aa,bb;
+    switch (a)
+    {
+    case '+':
+        aa=0;
+        break;
+    case '*':
+        aa=1;
+        break;
+    case 'i':
+        aa=2;
+        break;
+    case '(':
+        aa=3;
+        break;
+    case ')':
+        aa=4;
+        break;
+    case '#':
+        aa=5;
+        break;
+    default:
+        break;
+    }
+    switch (b)
+    {
+    case '+':
+        bb=0;
+        break;
+    case '*':
+        bb=1;
+        break;
+    case 'i':
+        bb=2;
+        break;
+    case '(':
+        bb=3;
+        break;
+    case ')':
+        bb=4;
+        break;
+    case '#':
+        bb=5;
+        break;
+    default:
+        break;
+    }
+    return matrix[aa][bb];
+}
 int main(int argc, char ** argv){
     FILE *fp=NULL;
     char ch;
-    char *s=NULL;
     //打开文件
     fp = fopen(argv[1],"r");
     //第一个双引号里面放的是文件具体路径，第二个是对文件进行的操作
-    while((ch=getc(fp))!=EOF){
-        if(ch=='+'){
-            puts("Plus");
-            continue;
-        }
-        else if(ch=='*'){
-            puts("Star");
-            continue;
-        }
-        else if(ch==','){
-            puts("Comma");
-            continue;
-        }
-        else if(ch=='('){
-            puts("LParenthesis");
-            continue;
-        }
-        else if(ch==')'){
-            puts("RParenthesis");
-            continue;
-        }
-        else if(ch==':'){
-            char ch1=getc(fp);
-            if(ch1=='='){
-                puts("Assign");
+    char op_stack[1050],sub_stack[1050];
+    int op_pointer=0,sub_pointer=-1;
+    op_stack[0]='#';
+    char s[1050];
+    int i=0;
+    while((ch=getc(fp))!='\n'){
+        s[i++]=ch;
+    }
+    s[i]=0,s[i-1]='#';
+    fclose(fp);
+    int read=0;
+    while(op_stack[op_pointer]!='#'||s[read]!='#'){
+        char curread=s[read],curop=op_stack[op_pointer];
+        switch (get_priority(curop,curread))
+        {
+        case -1:
+            if(curread=='i'){
+                sub_stack[++sub_pointer]=curread;
             }
-            else{
-                puts("Colon");
-                if(ch1==EOF){
+            op_stack[++op_pointer]=s[read++];
+            printf("I%c\n",curread);
+            break;
+        case 0:
+            read++;
+            op_pointer--;
+            break;
+        case 1:
+            if(curop=='i'){
+                if(sub_stack[sub_pointer]=='i'){
+                    sub_pointer--;
+                    sub_stack[++sub_pointer]='N';
+                    op_pointer--;
+                    puts("R");
+                }
+                else{
+                    puts("RE");
                     return 0;
                 }
-                fseek(fp,-1L,1);
             }
-            continue;
-        }
-        else if(isdigit(ch)){
-            int ans=ch-'0';
-            while((ch=getc(fp))!=EOF&&isdigit(ch)){
-                ans=ans*10+ch-'0';
-            }
-            printf("Int(%d)\n",ans);
-            fseek(fp,-1L,1);
-        }
-        else if(isalpha(ch)){
-            char s[500];
-            int i=0;
-            s[i++]=ch;
-            while((ch=getc(fp))!=EOF&&(isdigit(ch)||isalpha(ch))){
-                s[i++]=ch;
-            }
-            if(ch!=EOF){
-                fseek(fp,-1L,1);
-            }
-            s[i]=0;
-            if(strcmp(s,"BEGIN")==0||strcmp(s,"END")==0||strcmp(s,"FOR")==0||strcmp(s,"IF")==0||strcmp(s,"THEN")==0||
-            strcmp(s,"ELSE")==0){
-                for(int j=1;j<strlen(s);j++){
-                    s[j]=tolower(s[j]);
+            else if(curop=='+'||curop=='*'){
+                    if(sub_pointer>0&&sub_stack[sub_pointer]=='N'&&sub_stack[sub_pointer-1]=='N'){
+                        sub_pointer-=2;
+                        sub_stack[++sub_pointer]='N';
+                        op_pointer--;
+                        puts("R");
+                    }
+                    else{
+                        puts("RE");
+                        return 0;
                 }
-                puts(s);
             }
-            else{
-                printf("Ident(%s)\n",s);
+            else if(curop==')'){
+                    op_pointer--;
             }
-        }
-        else if(ch==' '||ch=='\n'||ch=='\t'){
-            continue;
-        }
-        else{
-            puts("Unknown");
+            break;
+        case 2:
+            puts("E");
+            return 0;
+            break;
+        default:
             break;
         }
     }
-    fclose(fp);
     return 0;
 }
